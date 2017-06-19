b4d990ec1433a3957354987362c7b6e32b4d9770
sarrablo <gsarrablo@gmail.com>
2017-06-19 19:55:38 +0200

    Added tokenw

--- FestiBlaCarBot.py
+++ FestiBlaCarBot.py
@@ -4,9 +4,12 @@ from telebot import types
 import time
 
 
-
-TOKEN = 'token'
-bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
+f  = open('credentials','r')
+token = f.read()
+f.close()
+print(token)
+TOKEN = token 
+bot = telebot.TeleBot('430791351:AAHC-M4rgsPLxfpeASMj3sz4Zy0bSMSgXdU') # Creamos el objeto de nuestro bot.
 print (bot.get_me())
 
 
