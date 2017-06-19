0f4bad60429979d62b0b358e56d114c4ef233638
sarrablo <gsarrablo@gmail.com>
2017-06-19 19:32:06 +0200

    Add imports

--- FestiBlaCarBot.py
+++ FestiBlaCarBot.py
@@ -1,1 +1,12 @@
 import FestiBlaCarDb
+import telebot
+from telebot import types
+import time
+from robobrowser import RoboBrowser
+from bs4 import BeautifulSoup
+
+TOKEN = 'token'
+bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
+print (bot.get_me())
+
+
