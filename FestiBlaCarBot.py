c6836fd0310d1f29ee0eedd90ea5c527616af6fb
sarrablo <gsarrablo@gmail.com>
2017-06-19 19:34:44 +0200

    deleted unnecesary imports

--- FestiBlaCarBot.py
+++ FestiBlaCarBot.py
@@ -2,8 +2,8 @@ import FestiBlaCarDb
 import telebot
 from telebot import types
 import time
-from robobrowser import RoboBrowser
-from bs4 import BeautifulSoup
+
+
 
 TOKEN = 'token'
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
